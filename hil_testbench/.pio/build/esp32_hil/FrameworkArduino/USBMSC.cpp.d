26da12662aa55c59470d0f771368c30eaebf95fb
mirabeauBerant <88736510+MirabeauBerant@users.noreply.github.com>
2026-05-20 12:37:31 -0500

    chore: agregar .gitignore de PlatformIO y limpiar build artifacts

--- hil_testbench/.pio/build/esp32_hil/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32_hil/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/MirabeauProBook/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/MirabeauProBook/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/MirabeauProBook/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
